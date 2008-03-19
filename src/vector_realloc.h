aa45042e11934e0937bd336c0551ef322d955594
Tommi Rantala <tt.rantala@gmail.com>
2008-03-19 12:02:23 +0000

    implement the vector ADT using realloc

---
+++ src/vector_realloc.h
@@ -0,0 +1,111 @@
+#ifndef VECTOR_REALLOC
+#define VECTOR_REALLOC
+
+template <typename T, unsigned InitialSize=16>
+class vector_realloc
+{
+public:
+	vector_realloc() : _data(0), _size(0), _capacity(0) {}
+	~vector_realloc() { if (_data) free(_data); }
+	void push_back(const T& t)
+	{
+		if (_size == _capacity) { grow(); }
+		_data[_size] = t;
+		++_size;
+	}
+	void clear()
+	{
+		if (_data) free(_data);
+		_data = 0;
+		_size = 0;
+		_capacity = 0;
+	}
+	const T* begin() const { return _data; }
+	const T* end() const  { return _data+_size; }
+	size_t capacity() const { return _capacity; }
+	size_t size() const     { return _size;     }
+private:
+	void grow()
+	{
+		_capacity <<= 1;
+		if (_capacity == 0) {
+			_capacity = InitialSize;
+		}
+		_data = static_cast<T*>(realloc(_data, _capacity*sizeof(T)));
+	}
+	T* _data;
+	size_t _size;
+	size_t _capacity;
+};
+
+template <typename T, unsigned InitialSize=16>
+class vector_realloc_counter_clear
+{
+public:
+	vector_realloc_counter_clear() : _data(0), _size(0), _capacity(0) {}
+	~vector_realloc_counter_clear() { if (_data) free(_data); }
+	void push_back(const T& t)
+	{
+		if (_size == _capacity) { grow(); }
+		_data[_size] = t;
+		++_size;
+	}
+	void clear() { _size=0; }
+	const T* begin() const  { return _data;       }
+	const T* end() const    { return _data+_size; }
+	size_t capacity() const { return _capacity;   }
+	size_t size() const     { return _size;       }
+private:
+	void grow()
+	{
+		_capacity <<= 1;
+		if (_capacity == 0) {
+			_capacity = InitialSize;
+		}
+		_data = static_cast<T*>(realloc(_data, _capacity*sizeof(T)));
+	}
+	T* _data;
+	size_t _size;
+	size_t _capacity;
+};
+
+template <typename T, unsigned InitialSize=16>
+class vector_realloc_shrink_clear
+{
+public:
+	vector_realloc_shrink_clear() : _data(0), _size(0), _capacity(0) {}
+	~vector_realloc_shrink_clear() { if (_data) free(_data); }
+	void push_back(const T& t)
+	{
+		if (_size == _capacity) { grow(); }
+		_data[_size] = t;
+		++_size;
+	}
+	void clear() { shrink(); }
+	const T* begin() const  { return _data;       }
+	const T* end() const    { return _data+_size; }
+	size_t capacity() const { return _capacity;   }
+	size_t size() const     { return _size;       }
+private:
+	void grow()
+	{
+		_capacity <<= 1;
+		if (_capacity == 0) {
+			_capacity = InitialSize;
+		}
+		_data = static_cast<T*>(realloc(_data, _capacity*sizeof(T)));
+	}
+	void shrink()
+	{
+		if (_capacity <= InitialSize) return;
+		_capacity = _capacity / 2;
+		_size = 0;
+		_data = static_cast<T*>(
+			realloc(_data, _capacity*sizeof(T*)));
+	}
+	T* _data;
+	size_t _size;
+	size_t _capacity;
+};
+
+#endif //VECTOR_REALLOC

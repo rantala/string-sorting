c8c6bcb7c84fd55dffef8b6258d0d98fa2620899
Tommi Rantala <tt.rantala@gmail.com>
2011-12-21 22:29:03 +0200

    Introduce msd_CE4 that uses 16-bit bucket counter type for small subinputs

--- src/msd_ce.cpp
+++ src/msd_ce.cpp
@@ -42,7 +42,16 @@
  *   - uses superalphabet when the size of the subinput is large, normal
  *     alphabet otherwise
  *
- * C3 is the fastest implementation in most cases.
+ * msd_CE4: Otherwise the same as CE0, except:
+ *   - uses O(n) oracle to reduce TLB and other cache misses
+ *   - avoids memory stalls by building oracle first, then calculating the size
+ *     of each bucket
+ *   - uses superalphabet when the size of the subinput is large, normal
+ *     alphabet otherwise
+ *   - uses 16-bit bucket counter type in msd_CE2 that is used for inputs
+ *     smaller than 0x10000
+ *
+ * CE4 is the fastest implementation in most cases.
  */
 
 #include "util/insertion_sort.h"
@@ -223,3 +232,42 @@ msd_CE3(unsigned char** strings, size_t n, size_t depth)
 
 void msd_CE3(unsigned char** strings, size_t n)
 { msd_CE3(strings, n, 0); }
+
+static void
+msd_CE4(unsigned char** strings, size_t n, size_t depth)
+{
+	if (n < 0x10000) {
+		msd_CE2_16bit(strings, n, depth);
+		return;
+	}
+	uint16_t* restrict oracle =
+		(uint16_t*) malloc(n*sizeof(uint16_t));
+	for (size_t i=0; i < n; ++i)
+		oracle[i] = get_char<uint16_t>(strings[i], depth);
+	size_t* restrict bucketsize = (size_t*)
+		calloc(0x10000, sizeof(size_t));
+	for (size_t i=0; i < n; ++i)
+		++bucketsize[oracle[i]];
+	unsigned char** sorted = (unsigned char**)
+		malloc(n*sizeof(unsigned char*));
+	static size_t bucketindex[0x10000];
+	bucketindex[0] = 0;
+	for (size_t i=1; i < 0x10000; ++i)
+		bucketindex[i] = bucketindex[i-1]+bucketsize[i-1];
+	for (size_t i=0; i < n; ++i)
+		sorted[bucketindex[oracle[i]]++] = strings[i];
+	memcpy(strings, sorted, n*sizeof(unsigned char*));
+	free(sorted);
+	free(oracle);
+	size_t bsum = bucketsize[0];
+	for (size_t i=1; i < 0x10000; ++i) {
+		if (bucketsize[i] == 0) continue;
+		if (i & 0xFF) msd_CE4(strings+bsum,
+				bucketsize[i], depth+2);
+		bsum += bucketsize[i];
+	}
+	free(bucketsize);
+}
+
+void msd_CE4(unsigned char** strings, size_t n)
+{ msd_CE4(strings, n, 0); }

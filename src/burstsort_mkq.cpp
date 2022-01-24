rantala/string-sorting

commit d4c45c9ac2c60511ff42bff4748d0e0458ab32fb

burstsort_mkq.cpp: fix burstsort_mkq_recursiveburst routine
Fix burstsort_mkq_recursiveburst routine: after recursive bursting, we
would access out-of-date entries from the oracle array. To fix it,
refresh the oracle when it is needed.

Thanks to Timo Bingmann <tbgit@panthema.net> for noticing the problem:
https://github.com/bingmann/parallel-string-sorting/commit/cfd3de7bd0284e8ef927804d1c92a7dfeb18ded9

src/burstsort_mkq.cpp
@@ -98,38 +98,32 @@ struct BurstRecursive
 				unsigned(0.7f*bucket.size()));
 		assert(bucket0->size() + bucket1->size() + bucket2->size()
 				== bucket.size());
-		size_t bsize = bucket0->size();
+		size_t bsize = 0;
 		if (bucket0->size() > threshold) {
+			for (unsigned i=0; i < bucket0->size(); ++i)
+				oracle[bsize+i] = get_char<CharT>((*bucket0)[i], depth);
 			new_node->buckets[0] = this->operator()(*bucket0,
 					oracle, depth);
 			delete bucket0;
 			new_node->is_tst[0] = true;
-		} else {
-			new_node->buckets[0] = bucket0;
-			assert(new_node->is_tst[0] == false);
 		}
+		bsize += bucket0->size();
 		if (bucket1->size() > threshold and not is_end(new_node->pivot)) {
-			for (unsigned i=0; i < bucket1->size(); ++i) {
-				oracle[bsize+i] = get_char<CharT>((*bucket1)[i],
-						depth+sizeof(CharT));
-			}
+			for (unsigned i=0; i < bucket1->size(); ++i)
+				oracle[bsize+i] = get_char<CharT>((*bucket1)[i], depth+sizeof(CharT));
 			new_node->buckets[1] = this->operator()(*bucket1,
 					oracle+bsize, depth+sizeof(CharT));
-			bsize += bucket1->size();
 			delete bucket1;
 			new_node->is_tst[1] = true;
-		} else {
-			new_node->buckets[1] = bucket1;
-			assert(new_node->is_tst[1] == false);
 		}
+		bsize += bucket1->size();
 		if (bucket2->size() > threshold) {
+			for (unsigned i=0; i < bucket2->size(); ++i)
+				oracle[bsize+i] = get_char<CharT>((*bucket2)[i], depth);
 			new_node->buckets[2] = this->operator()(*bucket2,
 					oracle+bsize, depth);
 			delete bucket2;
 			new_node->is_tst[2] = true;
-		} else {
-			new_node->buckets[2] = bucket2;
-			assert(new_node->is_tst[2] == false);
 		}
 		return new_node;
 	}

commit f22ee85679d28524370dcaf2987bdb945968d73b

burstsort_mkq.cpp: fix use-after-free regression
Do not call bucket0->size() after bucket0 is already deleted (ditto for
bucket1). Fixes use-after-free regression introduced in commit
d4c45c9ac2c60511ff42bff4748d0e0458ab32fb

src/burstsort_mkq.cpp
@@ -107,6 +107,8 @@ struct BurstRecursive
 		assert(bucket0->size() + bucket1->size() + bucket2->size()
 				== bucket.size());
 		size_t bsize = 0;
+		const size_t bsize0 = bucket0->size();
+		const size_t bsize1 = bucket1->size();
 		if (bucket0->size() > threshold) {
 			debug() << "BurstRecursive() bursting bucket0: size()=" << bucket0->size()
 				<< " threshold=" << threshold << " bsize=" << bsize << "\n";
@@ -117,7 +119,7 @@ struct BurstRecursive
 			delete bucket0;
 			new_node->is_tst[0] = true;
 		}
-		bsize += bucket0->size();
+		bsize += bsize0;
 		if (bucket1->size() > threshold and not is_end(new_node->pivot)) {
 			debug() << "BurstRecursive() bursting bucket1: size()=" << bucket1->size()
 				<< " threshold=" << threshold << " bsize=" << bsize << "\n";
@@ -128,7 +130,7 @@ struct BurstRecursive
 			delete bucket1;
 			new_node->is_tst[1] = true;
 		}
-		bsize += bucket1->size();
+		bsize += bsize1;
 		if (bucket2->size() > threshold) {
 			debug() << "BurstRecursive() bursting bucket2: size()=" << bucket2->size()
 				<< " threshold=" << threshold << " bsize=" << bsize << "\n";